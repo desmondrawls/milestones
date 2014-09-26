c9ccfccbcefbf183e303342a8557b39311b6dc18
Desmond Rawls <captaingrover@gmail.com>
2014-09-26 17:33:49 -0400

    Creates a join table for achievements. Cleans up routes a little bit. Style needs to be reworked.

--- main.rb
+++ main.rb
@@ -8,38 +8,34 @@ class Milestone
   include DataMapper::Resource
   property :id,           Serial
   property :name,         String, :required => true
-  property :reached_at,   DateTime
-  belongs_to :baby
+  has n, :babies, :through => :achievements
+  has n, :achievements, :constraint => :destroy
 end
 
 class Baby
   include DataMapper::Resource
   property :id,           Serial
   property :name,         String, :required => true
-  property :birthdate,    Date
-  has n, :milestones, :constraint => :destroy
+  property :birthdate,    Date, :required => true
+  property :gender,       String
+  has n, :milestones, :through => :achievements
+  has n, :achievements, :constraint => :destroy
+end
+
+class Achievement
+  include DataMapper::Resource
+  property :id,           Serial
+  property :reached_at,   DateTime
+  belongs_to :baby
+  belongs_to :milestone
 end
 DataMapper.finalize
 
+
 get '/' do
   @babies = Baby.all
+  @milestones = Milestone.all
   haml :index
-end
- 
-get '/baby/:id' do
-  @baby = Baby.get(params[:id])
-  @milestones = @baby.milestones.all
-  haml :baby
-end
-
-post '/baby/:id/milestones' do
-  Baby.get(params[:id]).milestones.create params['milestone']
-  redirect to("baby/#{params[:id]}")
-end
-
-get '/:milestone' do
-  @milestone = params[:milestone].split('-').join(' ').capitalize
-  haml :milestone
 end
 
 post '/' do
@@ -47,25 +43,54 @@ post '/' do
   redirect to('/')
 end
 
-delete '/milestone/:id' do
-  Milestone.get(params[:id]).destroy
-  redirect to('/')
-end
-
-put '/milestone/:id' do
-  milestone = Milestone.get params[:id]
-  milestone.reached_at = milestone.reached_at.nil? ? Time.now : nil
-  milestone.save
-  redirect to("/baby/#{milestone.baby.id}")
-end
-
-post '/new/baby' do
+post '/babies' do
   Baby.create params['baby']
   redirect to('/')
+end
+ 
+get '/baby/:id' do
+  @baby = Baby.get(params[:id])
+  @milestones = Milestone.all
+  @baby_achievements = @baby.achievements.all
+  haml :baby
 end
 
 delete '/baby/:id' do
   Baby.get(params[:id]).destroy
   redirect to('/')
 end
+
+post '/milestones' do
+  Milestone.create params['milestone']
+  redirect to("/")
+end
+
+get '/milestones/:id' do
+  "Under Construction"
+end
+
+delete '/milestones/:id' do
+  Milestone.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/baby/:id/achievements' do
+  milestone = Milestone.get params[:id]
+  milestone.reached_at = milestone.reached_at.nil? ? Time.now : nil
+  milestone.save
+  redirect to("/baby/#{milestone.baby.id}")
+end
+
+delete '/baby/:id/achievements' do
+  achievement = Achievement.all(:baby_id => params['id'], :milestone_id => params['milestone_id'])
+  achievement.destroy
+  redirect to("/baby/#{params['id']}")
+end
+
+post '/baby/:id/achievements' do
+  Achievement.create(:baby_id => params['id'], :milestone_id => params['milestone_id'], :reached_at => Time.now)
+  redirect to("/baby/#{params['id']}")
+end
+
+
  
